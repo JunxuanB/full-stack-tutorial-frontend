d1dcefd17ea97a8c818aba4ae593a6c4163bca3a
Junxuan Bao <Junxuan.Bao@Gmail.com>
2024-11-24 20:48:35 -0500

    update

--- src/components/Notice.tsx
+++ src/components/Notice.tsx
@@ -1,43 +1,62 @@
+import { Button } from "antd";
+import { useState } from "react";
+
 interface Props {
-    title: string;
-    content: string;
+  title: string;
+  content: string;
 }
 
 const Notice = (props: Props) => {
-    return (
-      <div
+  const [closed, setClosed] = useState(false);
+  return (
+    !closed && // 如果 closed 为 true 则不显示
+    <div
+      style={{
+        width: '50%', // 我在这里修改为了 50% 为了确保他可以被完整地显示
+        border: '1px dotted #aaa',
+        borderRadius: '5px',
+        padding: '10px',
+        position: 'relative',
+        backgroundColor: '#f9f9f9',
+        boxShadow: '0 2px 5px rgba(0, 0, 0, 0.1)',
+      }}
+    >
+      <h4
         style={{
-          width: '100%',
-          border: '1px dotted #aaa',
-          borderRadius: '5px',
-          padding: '10px',
-          position: 'relative',
-          backgroundColor: '#f9f9f9',
-          boxShadow: '0 2px 5px rgba(0, 0, 0, 0.1)',
+          margin: 0,
+          paddingBottom: '5px',
+          fontSize: '16px',
+          color: '#333',
+          borderBottom: '1px solid #ddd',
         }}
       >
-        <h4
-          style={{
-            margin: 0,
-            paddingBottom: '5px',
-            fontSize: '16px',
-            color: '#333',
-            borderBottom: '1px solid #ddd',
-          }}
+        {props.title}
+
+        {/* 使用 antd 的按钮 新增一个靠右的按钮 */}
+        <Button
+        type="link"
+        onClick={() => setClosed(!closed)}
+        style={{
+          position: 'absolute',
+          right: '0px',
+          top: '5px',
+          fontSize: '14px',
+        }}
         >
-            {props.title}
-        </h4>
-        <p
-          style={{
-            margin: '10px 0 0',
-            fontSize: '14px',
-            color: '#555',
-          }}
-        >
-            {props.content}
-        </p>
-      </div>
-    );
-  };
-  
+          隐藏
+        </Button>
+      </h4>
+      <p
+        style={{
+          margin: '10px 0 0',
+          fontSize: '14px',
+          color: '#555',
+        }}
+      >
+        {props.content}
+      </p>
+    </div>
+  );
+};
+
 export default Notice
--- src/pages/HelloWorld.tsx
+++ src/pages/HelloWorld.tsx
@@ -1,12 +1,14 @@
 import { useNavigate, useParams } from "react-router-dom";
 import Notice from "../components/Notice"
 import TemplateComponent from "../components/TemplateComponent";
+import { Button, Flex } from "antd";
 
 const HelloWorld = () => {
     const { id } = useParams();
     const navigate = useNavigate();
     return (
-        <>
+        <Flex vertical gap={'middle'}>
+            <Button type="primary">Hello World</Button>
             <button onClick={() => navigate('/temp/1')}>跳转到 /hello/2</button>
             <TemplateComponent>
                 <>
@@ -16,7 +18,7 @@ const HelloWorld = () => {
                     </p>
                 </>
             </TemplateComponent>
-        </>
+        </Flex>
     )
 }
 
